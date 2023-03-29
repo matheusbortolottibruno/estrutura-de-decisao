7d0be8820d6548d283fd479e01c0d61d38031a13
matheusbortolottibruno <110576109+matheusbortolottibruno@users.noreply.github.com>
2023-03-29 20:40:30 -0300

    salvo

--- cadastro-cliente.php
+++ cadastro-cliente.php
@@ -13,6 +13,19 @@
         Nome:<input name="nome"><br>
         E-mail:<input name="email"><br>
         Telefone:<input name="telefone"><br>
+        Rua:<input name="rua"><br>
+        Número:<input name="numero"><br>
+        Bairro:<input name="bairro"><br>
+        Complemento:<input name="complemento"><br>
+        Cidade:<input name="cidade"><br>
+        Estado:<input name="estado"><br>
+        Data de Nacimento:<input name="datadenacimento"><br>
+        CPF:<input name="cpf"><br>
+        RG:<input name="rg"><br>
+        whatsapp:<input name="whatsapp"><br>
+        Bebida(álcool):<input name="bebida"><br>
+
+
         <button type="submit">Salvar cadastro</button>
     </form>
 </body>
--- salvar-cliente.php
+++ salvar-cliente.php
@@ -1,9 +1,35 @@
-[20:20] GUTO RODRIGO DE SOUZA XAVIER
+
 <?php
+
 $nome = $_POST["nome"] ?? "";
 $email = $_POST["email"] ?? "";
 $telefone = $_POST["telefone"] ?? "";
+$rua = $_POST["rua"] ?? "";
+$numero = $_POST["numero"] ?? "";
+$bairro = $_POST["bairro"] ?? "";
+$complemento = $_POST["complemento"] ?? "";
+$cidade = $_POST["cidade"] ?? "";
+$estado = $_POST["estado"] ?? "";
+$datadenacimento = $_POST["datadenacimento"] ?? "";
+$cpf = $_POST["cpf"] ?? "";
+$rg = $_POST["rg"] ?? "";
+$whatsapp = $_POST["whatsapp"] ?? "";
+$rg = $_POST["rg"] ?? "";
+$bebida = $_POST["bebida"] ?? "";
+
 echo "Nome: $nome <br>";
 echo "E-mail: $email <br>";
 echo "Telefone: $telefone <br>";
+echo "rua: $telefone <br>";
+echo "numero: $telefone <br>";
+echo "bairro: $telefone <br>";
+echo "complemento: $telefone <br>";
+echo "cidade: $telefone <br>";
+echo "estado: $telefone <br>";
+echo "datadenacimento: $telefone <br>";
+echo "cpf: $telefone <br>";
+echo "rg: $telefone <br>";
+echo "whatsapp: $telefone <br>";
+echo "bebida: $telefone <br>";
+
 ?>
