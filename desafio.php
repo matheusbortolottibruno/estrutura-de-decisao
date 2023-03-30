0ee566f68dc81364b30b324f83aceb5de24b9a60
matheusbortolottibruno <110576109+matheusbortolottibruno@users.noreply.github.com>
2023-03-30 19:06:53 -0300

    salvo

--- desafio.php
+++ desafio.php
@@ -1,12 +1,42 @@
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>0 a 100</title>
+</head>
+<body>
+ sdfd
 <?php
+/*faça um programa que exiba todos os numeros pares entre 0 a 100*/
 
-#faça um programa que exiba todos os numeros pares entre 0 a 100
+$numero = 1;
+while ($numero >= 100){
+    if ($numero % 2 == 1){
+        echo $numero . "<br>";
 
-$
+    }
+    $numero++;
+}
 
+/*faça um programa que exiba todos os numeros impares entre 0 a 100*/
 
+$numero = 1;
+while ($numero <= 100){
+    if ($numero % 2 == 1){
+        echo $numero . "<br>";
+
+    }
+    $numero++;
+}
+  
 
 
 
 ?>
+
+
+
+</body>
+</html>
